8198046b04f051ee85cf966dfdc5415e5d2fc28f
tamasronto <144915216+tamasronto@users.noreply.github.com>
2023-10-03 13:24:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("titles").innerHTML= "Lebron"
+
+function changeColor() {
+    document.getElementById("titles").style.color = "blue"
+    console.log("de functie werkt");
+  }
+  
+
+  changeColor();
+  
